10e1ddcc4b3ebb46560ef4547aca29991791648f
Anantdevmore <76837118+Anantdevmore@users.noreply.github.com>
2021-09-09 08:50:57 +0530

    Create 1.c

---
+++ 1.c
@@ -0,0 +1,24 @@
+// OpenMP program to print Hello World
+// using C language
+
+// OpenMP header
+#include <omp.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[])
+{
+
+	// Beginning of parallel region
+	#pragma omp parallel
+	{
+
+		printf("Hello World... from thread = %d\n",
+	
+			omp_get_thread_num());
+	}
+	    printf("Hello Anant Devmore\n");
+		printf("Hello PRN NO-2018BTECS00110");
+	// Ending of parallel region
+}
